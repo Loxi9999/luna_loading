839fff3e9f4a3ac0e773763e82c7c662eda6b97f
Loxi <123603348+Loxi9999@users.noreply.github.com>
2024-07-18 00:31:31 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+loadscreen 'ui/index.html'
+loadscreen_manual_shutdown 'yes'
+
+files {
+  'ui/index.html',
+  'ui/effect-slicer.min.css',
+  'ui/effect-slicer.min.js',
+  'ui/logo.png',
+  'ui/main.css',
+  'ui/theme.mp3',
+  'ui/images/*.png'
+}
